8284b5cb147910aefa2da290619df66d96322f88
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-02-25 16:49:50 -0500

    added dayjs to find current date and hour

--- Develop/script.js
+++ Develop/script.js
@@ -16,6 +16,9 @@ $(function () {
 
        //$.each might need to use
       localStorage.setItem('id', id);
+
+      var currentTime = dayjs().format('dddd, MMMM D YYYY, hh')
+      console.log(currentTime);
       
   });
   });
@@ -26,9 +29,6 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  var currentTime = dayjs().format('dddd, MMMM D YYYY, h')
-  console.log(currentTime);
-
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
